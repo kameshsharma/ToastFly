edb09d0d41f0ea8dc17f73dfb0308cce94e7bb51
Kamesh Sharma <Kamesh1.Sharma@airtel.com>
2023-11-02 16:16:05 +0530

    commit added

---
+++ ToastMylibrary/src/main/java/com/example/toastmylibrary/MyToasterMessage.java
@@ -0,0 +1,12 @@
+package com.example.toastmylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MyToasterMessage {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
