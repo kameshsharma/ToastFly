f305ddc36600aed6b52e4c4c5ead51b8b36deaf2
Kamesh Sharma <Kamesh1.Sharma@airtel.com>
2023-11-09 14:34:42 +0530

    configure cmake changes

---
+++ ToastMylibrary/src/main/java/com/example/toastmylibrary/UniverseGuard.java
@@ -0,0 +1,39 @@
+package com.example.toastmylibrary;
+
+public class UniverseGuard {
+
+    static {
+        try {
+            System.loadLibrary("native-lib");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public int getHaveSu() {
+        return haveSu();
+    }
+
+    public int getHaveMagiskHide() {
+        return haveMagiskHide();
+    }
+
+    public int getHaveMagicMount() {
+        return haveMagicMount();
+    }
+
+    public boolean startFridaDetection(){
+        return detectFrida();
+    }
+
+    native int haveSu();
+
+    native int haveMagiskHide();
+
+    native int haveMagicMount();
+
+    native boolean detectFrida();
+
+    native void abortApp();
+
+}
---
+++ app/src/main/java/com/example/toasterfly/MainActivity2.java
@@ -0,0 +1,34 @@
+package com.example.toasterfly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.system.Os;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+//    static {
+//        try {
+//            System.loadLibrary("native-lib");
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+    TextView text1 ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UniversalGuardOne mMagiskDetector=new UniversalGuardOne();
+        text1 = this.findViewById(R.id.textv1);
+        Log.e("frida==>", String.valueOf(mMagiskDetector.startFridaDetection()));
+        int appId = Os.getuid() % 100000;
+        if (appId >= 10000) {
+            text1.setText(mMagiskDetector.detectFrida()+"\n"+String.valueOf(mMagiskDetector.getHaveSu())+"\n "+ String.valueOf(mMagiskDetector.getHaveMagicMount())+"\n "+String.valueOf(mMagiskDetector.getHaveMagiskHide()));
+            Log.e("magisk haveSu", String.valueOf(mMagiskDetector.getHaveSu()));
+            Log.e("magisk haveMagicMount", String.valueOf(mMagiskDetector.getHaveMagicMount()));
+            Log.e("magisk haveMagiskHide", String.valueOf(mMagiskDetector.getHaveMagiskHide()));
+        }
+    }
+}
